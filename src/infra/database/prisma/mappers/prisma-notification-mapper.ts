eecd52aa3eee040b7a2801cf544099be06ba056c
Marcus Vinicius <marcusviniciusfabr@gmail.com>
2022-12-16 22:54:55 -0300

    feat: add the send notification use case call by the send notification handle to save the notification

--- src/infra/database/prisma/mappers/prisma-notification-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -14,6 +14,7 @@ export class PrismaNotificationMapper {
       recipientId: notification.recipientId,
       readAt: notification.readAt,
       createdAt: notification.createdAt,
+      canceledAt: notification.canceledAt,
     };
   }
 
